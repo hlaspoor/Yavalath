ad6687578323b196c64d14d1e8c4745913a511d5
Junjie Yang <hlaspoor@gmail.com>
2016-01-24 20:36:16 +0800

    commit

--- js/eval.js
+++ js/eval.js
@@ -13,7 +13,7 @@ function Eval(ai) {
         0, 0, 0, 1, 9, 9, 9, 9, 1,
         0, 0, 0, 0, 1, 1, 1, 1, 1
     ];
-    this._attackVal = 300;
+    this._attackVal = 500;
 }
 
 Eval.prototype.check_dir_attack = function (idx, dir) {
